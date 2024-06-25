91fbac51c0092a5f33cc55aad134b61084d405e0
Andhika_R <101549113+KeyframeEditor@users.noreply.github.com>
2024-06-25 22:08:14 +0700

    BE Implementation pt1 - Add model

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template, request, jsonify
+import joblib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route()
+def detect_spam():
+    model = joblib.load('stack.model')
+    feature_extraction = joblib.load('feature_extraction.pkl')
+
+    def predict_spam(email_text):
+        input_mail_features = feature_extraction.transform(email_text)
+        prediction = model.predict(input_mail_features)
+
+        if prediction == 0:
+            return "HAM MAIL"
+        else:
+            return "SPAM MAIL"
+
+    # implement fungsi
+    input_mail = ["You're receiving this email because you turned on Location History, a Google Account-level setting that creates Timeline, a personal map of your visited places, routes, and trips."]
+    result = predict_spam(input_mail)
+    print(f'Text: {input_mail[0]}\nPrediction: {result}')
 
 # @app.route('/detect', methods=['POST'])
 # def detect_spam():
--- static/js/spamresult.js
+++ static/js/spamresult.js
@@ -1,19 +1,21 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alertPlaceholder = document.getElementById('OutputResult')
 const appendAlert = (message, type) => {
   const wrapper = document.createElement('div')
   wrapper.innerHTML = [
-    `<div class="alert alert-success alert-dismissible" role="alert">`,
-    `   <div><i class="bi bi-check-circle"></i> EMAIL TERDETEKSI HAM</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '<div class="alert alert-success alert-dismissible" role="alert">                             ',
+    '<div><i class="bi bi-check-circle"></i> EMAIL TERDETEKSI HAM</div>                           ',
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> ',
     '</div>'
   ].join('')
 
+  // For Detected SPAM
+  // '<div class="alert alert-danger alert-dismissible" role="alert">                               ',
+  // '<div><i class="bi bi-check-circle"></i> EMAIL TERDETEKSI SPAM</div>                           ',
+  // '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>  ',
+  // '</div>'
+
   alertPlaceholder.append(wrapper)
 }
-
-// buat yang spam
-// <div class="alert alert-danger alert-dismissible" role="alert
-// <div><i class="bi bi-x-circle"></i> EMAIL TERDETEKSI SPAM</div>
 
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
